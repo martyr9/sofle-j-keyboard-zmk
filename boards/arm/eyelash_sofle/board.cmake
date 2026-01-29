9d6aab7716a5b8e35e17af4332f17c491ae9f9b9
martyr9 <martin.richard.ca@protonmail.com>
2026-01-29 08:57:23 -0500

    Update board.cmake

--- boards/arm/eyelash_sofle/board.cmake
+++ boards/arm/eyelash_sofle/board.cmake
@@ -4,3 +4,5 @@
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+# ajout
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
